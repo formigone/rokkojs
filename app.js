97540a857f21fae8bf9f1c08d99f244a4e946748
Rodrigo Silveira <rsilveira@deseretdigital.com>
2013-10-08 16:08:47 -0600

    Refactored app.js using new factories

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ var thunder = 0;
 //    });
 //}
 
-function main(){
+function main() {
     var canvas = new rokko.components.DrawComponent();
     var renderer = new rokko.components.RendererComponent(canvas);
     canvas.show(document.body);
@@ -51,8 +51,8 @@ function main(){
     var spriteFactory = new rokko.factories.SpriteFactory();
     var entityFactory = new rokko.factories.EntityFactory(spriteFactory);
 
-    spriteFactory.loadFromJson("/config/megaman.sprites.json", function(factory){
-        entityFactory.loadFromJson("/config/megaman.entity.json", function(factory){
+    spriteFactory.loadFromJson("/config/megaman.sprites.json", function (factory) {
+        entityFactory.loadFromJson("/config/megaman.entity.json", function (factory) {
             soundFX.rain.volume = 0.5;
             soundFX.rain.play();
 
@@ -77,21 +77,21 @@ function main(){
             renderer.bwMode = false;
 
             // ON
-            setTimeout(function(){
+            setTimeout(function () {
                 renderer.bwMode = true;
-                setTimeout(function(){
+                setTimeout(function () {
 
                     // OFF
                     renderer.bwMode = false;
                     renderer.wonlyMode = true;
 
-                    setTimeout(function(){
+                    setTimeout(function () {
 
                         // ON
                         renderer.wonlyMode = false;
                         renderer.bwMode = true;
 
-                        setTimeout(function(){
+                        setTimeout(function () {
 
                             // OFF
                             renderer.bwMode = false;
@@ -102,366 +102,61 @@ function main(){
         }
     }
 
-    var EntityBehavior = function(){
-        goog.base(this, goog.events.EventTarget);
-    };
-    goog.inherits(EntityBehavior, goog.events.EventTarget);
+    function keyDown(e) {
+        var keyEvent = /** @type {goog.events.KeyEvent} */ (e);
 
-    // TODO: dispatch an actual event object, then let the listener handle this conditional logic
-    EntityBehavior.prototype.key = function(e) {
-        var key = e.keyCode;
-
-        if (key == 37) {
+        if (keyEvent.keyCode == goog.events.KeyCodes.LEFT) {
             e.preventDefault();
-            mm.sprite.setSprite("standing");
         }
 
-        if (key == 38) {
+        if (keyEvent.keyCode == goog.events.KeyCodes.UP) {
+            e.preventDefault();
+            if (keyEvent.shiftKey) {
+                mm.sprite.setSprite("spinning", true);
+            } else {
+                mm.sprite.setSprite("jumping", true);
+            }
+        }
+
+        if (keyEvent.keyCode == goog.events.KeyCodes.RIGHT) {
+            e.preventDefault();
+            if (keyEvent.shiftKey) {
+                mm.sprite.setSprite("running2", true);
+            } else {
+                mm.sprite.setSprite("running", true);
+            }
+        }
+
+        if (keyEvent.keyCode == goog.events.KeyCodes.DOWN) {
             e.preventDefault();
             mm.sprite.setSprite("standing", true);
         }
 
-        if (key == 39) {
+        if (keyEvent.keyCode == goog.events.KeyCodes.D) {
             e.preventDefault();
-            mm.sprite.setSprite("standing");
-        }
-
-        if (key == 40) {
-            e.preventDefault();
-            mm.sprite.setSprite("standing", true);
-        }
-
-        if (key == 68 /* D */) {
             canvas.setDebugMode(!canvas.isDebugMode());
         }
 
         // Demo thunder effect... obviously this is still very raw and manual... but there it is.
-        if (key == 84 /* T */) {
+        if (keyEvent.keyCode == goog.events.KeyCodes.T) {
             thunder = parseInt(Math.random() * soundFX.thunder.length);
             doThunder(thunder);
         }
-
-        this.dispatchEvent(EntityBehavior.EventType.KEY);
     };
 
-    EntityBehavior.EventType = {
-        KEY: goog.events.getUniqueId('key')
+    // TODO: Figure out how to register for onKeyUp
+    function keyUp(e) {
+        var keyEvent = /** @type {goog.events.KeyEvent} */ (e);
+        switch (keyEvent.keyCode) {
+            case goog.events.KeyCodes.UP :
+            case goog.events.KeyCodes.RIGHT:
+                mm.sprite.setSprite("standing", true);
+                break;
+        }
     };
-
-    var entityBeh = new EntityBehavior();
-    entityBeh.addEventListener(EntityBehavior.EventType.KEY, function(e){
-        console.log("Event: " + e);
-    });
 
     var keyHandler = new goog.events.KeyHandler(document.body);
-    goog.events.listen(keyHandler, goog.events.KeyHandler.EventType.KEY, entityBeh.key);
-
-//    var running = new rokko.graphics.SequencedImage("/img/8bitmm.gif", {
-//        frames: [
-//            {
-//                pos: {
-//                    x: 190,
-//                    y: 11
-//                },
-//                size: {
-//                    w: 24,
-//                    h: 24
-//                },
-//                freq: 100
-//            },
-//            {
-//                pos: {
-//                    x: 220,
-//                    y: 11
-//                },
-//                size: {
-//                    w: 16,
-//                    h: 24
-//                },
-//                freq: 100
-//            },
-//            {
-//                pos: {
-//                    x: 241,
-//                    y: 11
-//                },
-//                size: {
-//                    w: 21,
-//                    h: 24
-//                },
-//                freq: 100
-//            }
-//        ],
-//        freq: 110,
-//        currFrame: 0
-//    });
-//
-//    var running2 = new rokko.graphics.SequencedImage("/img/8bitmm.gif", {
-//        frames: [
-//            {
-//                pos: {
-//                    x: 144,
-//                    y: 269
-//                },
-//                size: {
-//                    w: 24,
-//                    h: 25
-//                },
-//                freq: 100
-//            },
-//            {
-//                pos: {
-//                    x: 174,
-//                    y: 269
-//                },
-//                size: {
-//                    w: 17,
-//                    h: 25
-//                },
-//                freq: 100
-//            },
-//            {
-//                pos: {
-//                    x: 197,
-//                    y: 269
-//                },
-//                size: {
-//                    w: 22,
-//                    h: 25
-//                },
-//                freq: 100
-//            }
-//        ],
-//        freq: 110,
-//        currFrame: 0
-//    });
-//
-//    var spinning = new rokko.graphics.SequencedImage("/img/8bitmm.gif", {
-//        frames: [
-//            {
-//                pos: {
-//                    x: 267,
-//                    y: 4
-//                },
-//                size: {
-//                    w: 26,
-//                    h: 31
-//                },
-//                freq: 75
-//            },
-//            {
-//                pos: {
-//                    x: 241,
-//                    y: 154
-//                },
-//                size: {
-//                    w: 21,
-//                    h: 31
-//                },
-//                freq: 75
-//            },
-//            {
-//                pos: {
-//                    x: 268,
-//                    y: 154
-//                },
-//                size: {
-//                    w: 26,
-//                    h: 31
-//                },
-//                freq: 75
-//            },
-//            {
-//                pos: {
-//                    x: 298,
-//                    y: 154
-//                },
-//                size: {
-//                    w: 22,
-//                    h: 31
-//                },
-//                freq: 75
-//            }
-//        ],
-//        freq: 100,
-//        currFrame: 0
-//    });
-//
-//    var jumping = new rokko.graphics.SequencedImage("/img/8bitmm.gif", {
-//        frames: [
-//            {
-//                pos: {
-//                    x: 267,
-//                    y: 4
-//                },
-//                size: {
-//                    w: 26,
-//                    h: 31
-//                },
-//                freq: 100
-//            }
-//        ],
-//        freq: 100,
-//        currFrame: 0
-//    });
-//
-//    var sprites = {
-//        standing: new rokko.graphics.Sprite(standing),
-//        running: new rokko.graphics.Sprite(running),
-//        running2: new rokko.graphics.Sprite(running2),
-//        jumping: new rokko.graphics.Sprite(jumping),
-//        spinning: new rokko.graphics.Sprite(spinning)
-//    };
-//
-//    var animFrames = {
-//        0: new rokko.graphics.Sprite(running),
-//        1: new rokko.graphics.Sprite(running2),
-//        2: new rokko.graphics.Sprite(standing),
-//        3: new rokko.graphics.Sprite(jumping),
-//        4: new rokko.graphics.Sprite(spinning)
-//    };
-//
-//    var animSprites = new rokko.graphics.AnimatedSprite({
-//        defaultFrame: 0,
-//        sprites: animFrames
-//    });
-//
-//    var animSprites2 = new rokko.graphics.AnimatedSprite({
-//        defaultFrame: 2,
-//        sprites: animFrames
-//    });
-//
-//    var hero2 = new rokko.entities.Entity({x: 300, y: 150}, {w: null, h: null, s: 3.5}, sprites.running);
-//    var hero3 = new rokko.entities.Entity({x: 450, y: 150}, {w: null, h: null, s: 3.5}, sprites.jumping);
-//    var hero4 = new rokko.entities.Entity({x: 450, y: 300}, {w: null, h: null, s: 3.5}, sprites.spinning);
-//    var hero5 = new rokko.entities.Entity({x: 300, y: 300}, {w: null, h: null, s: 3.5}, animSprites);
-//    var hero6 = new rokko.entities.Entity({x: 600, y: 200}, {w: null, h: null, s: 5.0}, animSprites2);
-//
-//    var canvas = new rokko.components.DrawComponent();
-//    var renderer = new rokko.components.RendererComponent(canvas);
-//
-//    canvas.setDebugMode(true);
-//    canvas.show(document.body);
-//
-//    genSprite('demo', 'standing', func);
-//
-//    renderer.addEntity(hero2);
-//    renderer.addEntity(hero3);
-//    renderer.addEntity(hero4);
-//    renderer.addEntity(hero5);
-//    renderer.addEntity(hero6);
-//
-//    (function chg(){
-//        var rnd = parseInt(Math.random() * 5);
-//        hero5.sprite.setSprite(rnd);
-//        setTimeout(function(){
-//            chg();
-//        }, 2500);
-//    })();
-//
-//
-//    var EntityBehavior = function(){
-//        goog.base(this, goog.events.EventTarget);
-//    };
-//    goog.inherits(EntityBehavior, goog.events.EventTarget);
-//
-//    // TODO: dispatch an actual event object, then let the listener handle this conditional logic
-//    EntityBehavior.prototype.key = function(e) {
-//        var key = e.keyCode;
-//
-//        if (key == 37) {
-//            e.preventDefault();
-//            hero6.sprite.setSprite(2);
-//        }
-//
-//        if (key == 38) {
-//            e.preventDefault();
-//            hero6.sprite.setSprite(3, true);
-//        }
-//
-//        if (key == 39) {
-//            e.preventDefault();
-//            hero6.sprite.setSprite(0);
-//        }
-//
-//        if (key == 40) {
-//            e.preventDefault();
-//            hero6.sprite.setSprite(4, true);
-//        }
-//
-//        if (key == 68 /* D */) {
-//            canvas.setDebugMode(!canvas.isDebugMode());
-//        }
-//
-//        // Demo thunder effect... obviously this is still very raw and manual... but there it is.
-//        if (key == 84 /* T */) {
-//            doThunder(thunder);
-//        }
-//
-//        this.dispatchEvent(EntityBehavior.EventTYpe.KEY);
-//    };
-//
-//    EntityBehavior.EventTYpe = {
-//        KEY: goog.events.getUniqueId('key')
-//    };
-//
-//    var entityBeh = new EntityBehavior();
-//    entityBeh.addEventListener(EntityBehavior.EventTYpe.KEY, function(e){
-//        console.log("Event: " + e);
-//    });
-//
-//    var keyHandler = new goog.events.KeyHandler(document.body);
-//    goog.events.listen(keyHandler, goog.events.KeyHandler.EventType.KEY, entityBeh.key);
-//
-//    function go(time) {
-//        soundFX.rain.volume = 0.5;
-//        soundFX.rain.play();
-//
-//        thunder = parseInt(Math.random() * soundFX.thunder.length);
-//        doThunder(thunder);
-//
-//        renderer.exec(time);
-//        requestAnimationFrame(go);
-//    }
-//
-//    function func(sprite){
-//        var hero = new rokko.entities.Entity({x: 100, y: 150}, {w: null, h: null, s: 7.5}, sprite);
-//        renderer.addEntity(hero);
-//    }
-//
-//    go(0);
-//
-//    function doThunder(thunder) {
-//        if (soundFX.thunder[thunder].paused || false) {
-//            soundFX.thunder[thunder].volume = 1.00;
-//            soundFX.thunder[thunder].play();
-//            renderer.bwMode = false;
-//
-//            // ON
-//            setTimeout(function(){
-//                renderer.bwMode = true;
-//                setTimeout(function(){
-//
-//                    // OFF
-//                    renderer.bwMode = false;
-//                    renderer.wonlyMode = true;
-//
-//                    setTimeout(function(){
-//
-//                        // ON
-//                        renderer.wonlyMode = false;
-//                        renderer.bwMode = true;
-//
-//                        setTimeout(function(){
-//
-//                            // OFF
-//                            renderer.bwMode = false;
-//                        }, 100);
-//                    }, 100);
-//                }, 100);
-//            }, 800);
-//        }
-//    }
+    goog.events.listen(keyHandler, goog.events.KeyHandler.EventType.KEY, keyDown);
 }
 
 goog.exportSymbol("main", main);

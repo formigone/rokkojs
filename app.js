01f62ba3cbf11c423a917088b2a757854496f607
Rodrigo Silveira <rsilveira@deseretdigital.com>
2013-10-09 05:54:48 -0600

    event handling with keyHandler doesn't seem to access keyup.

--- app.js
+++ app.js
@@ -13,16 +13,6 @@ goog.require("rokko.factories.EntityFactory");
 goog.require("goog.events.KeyHandler");
 goog.require("goog.net.XhrIo");
 
-var soundFX = {
-    rain: new Audio("/sound/rain.mp3"),
-    thunder: [
-//        new Audio("/sound/thunder.mp3"),
-        new Audio("/sound/thunder2.mp3"),
-        new Audio("/sound/thunder3.mp3")
-    ]
-};
-var thunder = 0;
-
 // TODO: implement way to override component methods per instance >> maybe a simple member assign through mutator does the trick??
 
 function main() {
@@ -36,9 +26,6 @@ function main() {
 
     spriteFactory.loadFromJson("/config/megaman.sprites.json", function (factory) {
         entityFactory.loadFromJson("/config/megaman.entity.json", function (factory) {
-            soundFX.rain.volume = 0.5;
-            soundFX.rain.play();
-
             mm = factory.make("megaman");
             renderer.addEntity(mm);
             _go(0);
@@ -50,84 +37,39 @@ function main() {
         requestAnimationFrame(_go);
     }
 
-
-    function doThunder(thunder) {
-        var sound = soundFX.thunder[thunder];
-        if (sound.paused) {
-            sound.startTime = 0.00;
-            sound.volume = 1.00;
-            sound.play();
-            renderer.bwMode = false;
-
-            // ON
-            setTimeout(function () {
-                renderer.bwMode = true;
-                setTimeout(function () {
-
-                    // OFF
-                    renderer.bwMode = false;
-                    renderer.wonlyMode = true;
-
-                    setTimeout(function () {
-
-                        // ON
-                        renderer.wonlyMode = false;
-                        renderer.bwMode = true;
-
-                        setTimeout(function () {
-
-                            // OFF
-                            renderer.bwMode = false;
-                        }, 100);
-                    }, 100);
-                }, 100);
-            }, 800);
-        }
-    }
-
     function keyDown(e) {
-        var keyEvent = /** @type {goog.events.KeyEvent} */ (e);
 
-        if (keyEvent.keyCode == goog.events.KeyCodes.LEFT) {
-            e.preventDefault();
-        }
+        switch (e.keyCode) {
+            case goog.events.KeyCodes.UP:
+                e.preventDefault();
+                if (e.shiftKey) {
+                    mm.sprite.setSprite("spinning", true);
+                } else {
+                    mm.sprite.setSprite("jumping", true);
+                }
+                break;
 
-        if (keyEvent.keyCode == goog.events.KeyCodes.UP) {
-            e.preventDefault();
-            if (keyEvent.shiftKey) {
-                mm.sprite.setSprite("spinning", true);
-            } else {
-                mm.sprite.setSprite("jumping", true);
-            }
-        }
+            case goog.events.KeyCodes.RIGHT:
+                e.preventDefault();
+                if (e.shiftKey) {
+                    mm.sprite.setSprite("running2", true);
+                } else {
+                    mm.sprite.setSprite("running", true);
+                }
+                break;
 
-        if (keyEvent.keyCode == goog.events.KeyCodes.RIGHT) {
-            e.preventDefault();
-            if (keyEvent.shiftKey) {
-                mm.sprite.setSprite("running2", true);
-            } else {
-                mm.sprite.setSprite("running", true);
-            }
-        }
+            case goog.events.KeyCodes.DOWN:
+                e.preventDefault();
+                mm.sprite.setSprite("standing", true);
+                break;
 
-        if (keyEvent.keyCode == goog.events.KeyCodes.DOWN) {
-            e.preventDefault();
-            mm.sprite.setSprite("standing", true);
-        }
-
-        if (keyEvent.keyCode == goog.events.KeyCodes.D) {
-            e.preventDefault();
-            canvas.setDebugMode(!canvas.isDebugMode());
-        }
-
-        // Demo thunder effect... obviously this is still very raw and manual... but there it is.
-        if (keyEvent.keyCode == goog.events.KeyCodes.T) {
-            thunder = parseInt(Math.random() * soundFX.thunder.length);
-            doThunder(thunder);
+            case goog.events.KeyCodes.D:
+                e.preventDefault();
+                canvas.setDebugMode(!canvas.isDebugMode());
+                break;
         }
     };
 
-    // TODO: Figure out how to register for onKeyUp
     function keyUp(e) {
         var keyEvent = /** @type {goog.events.KeyEvent} */ (e);
         switch (keyEvent.keyCode) {
@@ -138,8 +80,8 @@ function main() {
         }
     };
 
-    var keyHandler = new goog.events.KeyHandler(document.body);
-    goog.events.listen(keyHandler, goog.events.KeyHandler.EventType.KEY, keyDown);
+    goog.events.listen(document.body, goog.events.EventType.KEYDOWN, keyDown);
+    goog.events.listen(document.body, goog.events.EventType.KEYUP, keyUp);
 }
 
 goog.exportSymbol("main", main);

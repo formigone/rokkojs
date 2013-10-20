b0c603b68ea9c3e7cd2f4fdf9d21bf9130f0da12
Rodrigo Silveira <rsilveira@deseretdigital.com>
2013-10-20 03:08:17 -0600

    Added rokko map style

--- tools/rokko-map/js/modules/RokkoMap.js
+++ tools/rokko-map/js/modules/RokkoMap.js
@@ -1,1 +1,46 @@
-angular.module("RokkoMap", []);
+var rokkoMap = angular.module("RokkoMap", []);
+
+rokkoMap.controller("MenuController", function ($rootScope) {
+   $rootScope.actions = {
+      File: [
+         {id: "NEW", name: "New"},
+         {id: "OPEN", name: "Open"},
+         {id: "EXPORT", name: "Export"},
+         {id: "SETTINGS", name: "Settings"}
+      ],
+      Help: [
+         {id: "ABOUT", name: "About Rokko Map"}
+      ]
+   };
+
+   $rootScope.dispatchMenu = function (option) {
+      $rootScope.$broadcast("MenuEvent", option);
+   };
+});
+
+rokkoMap.controller("MapController", function ($scope) {
+   $scope.title = "No map selected";
+   $scope.width = 0;
+   $scope.height = 0;
+
+   $scope.setGrid = function (x, y) {
+      $scope.width = x;
+      $scope.height = y;
+   };
+
+   $scope.$on("MenuEvent", function (event, option) {
+      switch (option) {
+         case "NEW":
+            $scope.setGrid(23, 42);
+            break;
+      }
+   });
+});
+
+rokkoMap.controller("SettingsController", function ($scope) {
+   $scope.tile = {
+      width: 32,
+      height: 32,
+      margin: 0
+   };
+});
